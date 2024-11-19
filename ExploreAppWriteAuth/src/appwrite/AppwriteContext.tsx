be17fc968422bd2610b0baf42c044f6ca2168038
Hafsa-Begum <86627326+Hafsa-Begum@users.noreply.github.com>
2024-11-19 23:13:56 +0600

    add create context

---
+++ ExploreAppWriteAuth/src/appwrite/AppwriteContext.tsx
@@ -0,0 +1,31 @@
+import React, { createContext, FC, PropsWithChildren, useState } from 'react'
+import AppWrite from './service'
+
+type AppContextType = {
+    appwrite: AppWrite,
+    isLoggedIn: boolean,
+    setIsLoggedIn: (isLoggedIn:boolean)=> void
+}
+
+export const AppwriteContext = createContext<AppContextType>({
+    appwrite: new AppWrite(),
+    isLoggedIn: false,
+    setIsLoggedIn: ()=>{}
+})
+
+export const AppwriteProvider : FC<PropsWithChildren> = ({children})=> {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const defaultValue = {
+        appwrite: new AppWrite(),
+        isLoggedIn,
+        setIsLoggedIn
+    }
+
+    return (
+        <AppwriteContext.Provider value={defaultValue}>
+            {children}
+        </AppwriteContext.Provider>
+    )
+}
+
+export default AppwriteContext
